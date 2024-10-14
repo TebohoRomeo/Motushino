3086a77fc622986db9b0e05fe33c5e96314ccea9
TebohoRomeo <teboholekhalo13@gmail.com>
2024-10-14 13:32:49 +0200

    hino finance and dynamic pages

--- assets/js/hino-trucks.js
+++ assets/js/hino-trucks.js
@@ -1,74 +1,118 @@
-let trucks = [
+const data = {
+  series: [
     {
-        name: "Hino 200",
-        description: "RELIABLE AND TOUGH, PERFECT FOR MEDIUM AND LONG HAUL DISTANCES",
-        specs: [
-          { Engine_capacity: '2989', Transmission: 'Manual', Emission_Level: 'Euro 2', Application: '' , Front_Axle_Type: '', Rear_Axle_Type: '' }
-        ],
-        image: "truck_b.jpg"
-      },
-      {
-        name: "Hino 300 614 LWB",
-        description: "RELIABLE AND SAFE, THERE IS NO QUESTION THAT THE HINO 300 WILL GET YOUR GOODS SAFELY TO THEIR DESTINATION",
-        specs: [
-          { Engine_capacity: '2989', Transmission: 'MEchanical', Emission_Level: 'Euro 3', Application: 'Freight Carrier' , Front_Axle_Type: 'Reversed Elliot I-Beam', Rear_Axle_Type: 'Single Reducion Hypoid' }
-        ],
-        image: "truck_b.jpg"
-      },
-    {
-      name: "Truck B",
-      description: "Description of Truck A",
-      image: "truck_a.jpg"
+      name: "Hino 200 Series",
+      models: [
+        {
+          name: "Hino 200 310",
+          price: "R409,780 Excl VAT",
+          image: "./assets/img/product/hino-model-150-main.png",
+        },
+      ],
     },
-    // Add more trucks as needed
-  ];
-
-  let hino_200 = [
     {
-        name: "Hino 200",
-        description: "RELIABLE AND TOUGH, PERFECT FOR MEDIUM AND LONG HAUL DISTANCES",
-        specs: [
-          { Engine_capacity: '2989', Transmission: 'Manual', Emission_Level: 'Euro 2', Application: '' , Front_Axle_Type: '', Rear_Axle_Type: '' }
-        ],
-        image: "truck_b.jpg"
-      },
-    // Add more trucks as needed
-  ];
+      name: "Hino 300 Series",
+      models: [
+        {
+          name: "Hino 300 614 SWB MT",
+          price: "R582,498 Excl VAT",
+          image: "./assets/img/product/614-AT-SWB.jpg",
+        },
+        {
+          name: "Hino 300 614 SWB AT",
+          price: "R606,096 Excl VAT",
+          image: "./assets/img/product/614-AT-SWB.jpg",
+        },
+        {
+          name: "Hino 300 614 LWB MT",
+          price: "R593,676 Excl VAT",
+          image: "./assets/img/product/HINO 700-2841 FC AMT 6X4.png",
+        },
+        {
+          name: "Hino 300 714 SWB AT",
+          price: "R649,624 Excl VAT",
+          image: "./assets/img/product/714-SWB.jpg",
+        },
+      ],
+    },
+    {
+      name: "Hino 700 Series",
+      models: [
+        {
+          name: "Hino 700 2841 Tip SSC (AMT)",
+          price: "R1,674,720 Excl VAT",
+          image: "./assets/img/product/HINO 700-2841 FC AMT 6X4.png",
+        },
+        {
+          name: "Hino 700 2841 FC SSC (AMT)",
+          price: "R1,584,740 Excl VAT",
+          image: "./assets/img/product/HINO 700-2841 FC AMT 6X4.png",
+        },
+        {
+          name: "Hino 700 2841 TT SSC 3.9 Stl (AMT)",
+          price: "R1,643,720 Excl VAT",
+          image: "./assets/img/product/HINO 700-2841 FC AMT 6X4.png",
+        },
+        {
+          name: "Hino 700 2841 TT SSC 3.9 Stl (AMT)",
+          price: "R2,060,559 Excl VAT",
+          image: "./assets/img/product/HINO 700-2841 FC AMT 6X4.png",
+        },
+      ],
+    },
+  ],
+};
 
-  let truckListContainer = document.getElementById("truckList");
+// Rendering the HTML from JSON data
+const contentDiv = document.getElementById("content");
 
-  trucks.forEach((truck, index) => {
-    let truckDiv = document.createElement("div");
-    truckDiv.classList.add("truck-item");
+data.series.forEach((series) => {
+  // Add series title
+  const seriesTitleDiv = document.createElement("div");
+  seriesTitleDiv.classList.add("col-12");
+  seriesTitleDiv.innerHTML = `<h2 class="mb-5">${series.name}</h2>`;
+  contentDiv.appendChild(seriesTitleDiv);
 
-    let nameElement = document.createElement("h2");
-    nameElement.textContent = truck.name;
+  // Add models
+  series.models.forEach((model) => {
+    const modelDiv = document.createElement("div");
+    modelDiv.classList.add("col-lg-4", "col-12", "mb-3");
 
-    let descriptionElement = document.createElement("p");
-    descriptionElement.textContent = truck.description;
+    modelDiv.innerHTML = `
+                    <div class="product-thumb">
+                        <a href="#">
+                            <img src="${model.image}" class="img-fluid product-image" alt="${model.name}">
+                        </a>
+                        <div class="product-info d-flex">
+                            <div>
+                                <h5 class="product-title mb-0">
+                                    <a href="#" class="product-title-link">${model.name}</a>
+                                </h5>
+                                <p class="product-p">${model.price}</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
 
-    let specsElement = document.createElement("ul");
-    if (truck.specs) {
-      truck.specs.forEach(spec => {
-        let specItem = document.createElement("li");
-        specItem.textContent = `${Object.keys(spec)[0]}: ${Object.values(spec)[0]}`;
-        specsElement.appendChild(specItem);
-      });
-    }
-
-    let imageElement = document.createElement("img");
-    imageElement.src = `images/${truck.image}`;
-
-    // Create a link to truck details page
-    let truckLink = document.createElement("a");
-    truckLink.href = `./hino-details-single.html?index=${index}`; // Pass index or any unique identifier
-
-    truckLink.appendChild(nameElement);
-    truckDiv.appendChild(truckLink);
-    truckDiv.appendChild(descriptionElement);
-    truckDiv.appendChild(specsElement);
-    truckDiv.appendChild(imageElement);
-
-    truckListContainer.appendChild(truckDiv);
+    contentDiv.appendChild(modelDiv);
+  });
 });
+
+
+
+
+// <section class="products section-padding">
+// <div class="container">
+//     <div id="content" class="row">
+//         <!-- Content will be dynamically inserted here -->
+//     </div>
+// <!-- REtuen the html here -->
+// </div>
+// </section>
+
+
+
+
+
+
 
